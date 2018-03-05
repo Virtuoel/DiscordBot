5ade154d050b2417da977eed69ceb07a188d904e
Virtuoel <Virtuoel@users.noreply.github.com>
2018-03-05 21:12:31 +0300

    Fix stop command

--- modules/admin/kill.rb
+++ modules/admin/kill.rb
@@ -3,8 +3,7 @@ def exec_stop(event)
 	return nil unless is_admin(event.user)
 	send_temp_msg(event, "Goodbye.", true, 2)
 	sleep(1)
-	on_stop()
-	exit
+	$bot.stop
 end
 
 $bot.command(:kill, help_available: false) do |event|
